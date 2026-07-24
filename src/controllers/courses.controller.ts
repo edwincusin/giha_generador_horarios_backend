9476e0bd72d0e0bfa00df5a0b41b182b2c40809e
EDWINCSN <c.edwin32@hotmail.com>
2026-07-23 23:49:48 -0500

    feat(courses.controller.ts): agregar endpoint createCourse

--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from "express";
+import { type Request, type Response, type NextFunction, response } from "express";
 import prisma from "../database/prisma.js";
 
 //POINT GET DE TODAS LAS MATERIAS
@@ -16,6 +16,7 @@ export const getAllCourses = async (req: Request, res: Response) => {
 
         res.status(200).json(courses);
     } catch (error) {
+        console.error(error);
         res
             .status(500)
             .json({ error: " Error al ejecutar endpoint getAllCourses" });
@@ -37,8 +38,39 @@ export const getCourseById = async (req: Request, res: Response) => {
 
         res.status(200).json(course);
     } catch (error) {
+        console.error(error);
         res
             .status(500)
             .json({ error: " Error al ejecutar endpoint getCourseById" });
     }
 };
+
+
+//ENPOINT POST: CREAR UNA MATERIA
+export const createCourse = async (req: Request, res: Response) => {
+    try {
+        const { name, day, start_time, end_time, modality, difficulty, credits } = req.body
+
+        if (!name || !day || !start_time || !end_time || !modality || !difficulty || !credits) {
+            return res.status(400).json({ mensaje: "Todos los campos son obligatorios" });
+        }
+
+        const newCourse=await prisma.courses.create({
+            data:{
+                name,
+                day,
+                start_time:new Date(`1970-01-01T${ start_time }:00.000Z`),
+                end_time:new Date(`1970-01-01T${ end_time }:00.000Z`),
+                modality,
+                difficulty,
+                credits:Number(credits)
+            }
+        });
+
+        res.status(201).json({mensaje:"Materia creada con exito."})
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: " Error al ejecutar endpoint createCourse" });
+    }
+}
+
