89a5c0d9e6f8abe3d9c6b44386458852d682636c
EDWINCSN <c.edwin32@hotmail.com>
2026-07-27 21:27:15 -0500

    feat(prerequisites.controller.ts): agregar endpoint getAllPrerequisites

---
+++ src/controllers/preriquisites.controller.ts
@@ -0,0 +1,19 @@
+import type { Request, Response } from "express";
+import prisma from "../database/prisma.js";
+
+
+//METODO PARA CONSULTAR TODOS LOSPRERREQUISITOS REGISTRADOS
+export const getAllPrerequisites=async(req:Request, res:Response)=>{
+
+    try {
+        const prerequisites=await prisma.prerequisites.findMany(
+            {
+                orderBy :{course_id:"asc"}
+            }
+        );
+        res.status(200).json(prerequisites);
+    } catch (error) {
+        console.error("Error al recuperar getAllPrerequisites: ",error)
+        res.status(500).json({error:"Error al recuperar getAllPrerequisites"});
+    }
+} 
