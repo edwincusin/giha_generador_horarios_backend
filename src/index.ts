0829e4ec7cc38116543278b09f2c5a0640a64b3c
EDWINCSN <c.edwin32@hotmail.com>
2026-07-30 02:10:27 -0500

    feat(index.ts): montar router de schedules bajo /api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import routerCourses from './routes/courses.router.js';
 import routerPrerequisites from './routes/prerequisites.router.js';
 import routerScheduleConfigurations from './routes/scheduleConfigurations.router.js';
+import routerSchedules from './routes/schedule.ruter.js'
+
 
 import cors from 'cors';
 
@@ -13,6 +15,7 @@ app.use(express.json());
 app.use("/api",routerCourses);
 app.use("/api",routerPrerequisites);
 app.use("/api", routerScheduleConfigurations);
+app.use("/api", routerSchedules);
 
 
 app.listen(PORT,()=>{
